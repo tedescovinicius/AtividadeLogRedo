86e3c898748c6ebb00ba3628a7d1b8613892e326
jeferson <50223459+liftt1@users.noreply.github.com>
2022-02-13 16:56:35 -0300

    add update

--- index.php
+++ index.php
@@ -5,20 +5,29 @@ $abertas = [];
 $to_redo = [];
 $Checkpoint = [];
 
+if (!$db_handle) {
+    echo "An error occurred.\n";
+    exit;
+}
+
 while(!feof($arquivo)){
     $linha = fgets($arquivo, 1024);
     $linha = (str_replace('>','',str_replace('<','',explode(' ',$linha))));
+
     if($linha[0] == "Start"){ //Checkpoint
-        array_push($Checkpoint, str_replace( ')','', str_replace('(','',$linha[2])));
+        array_push($Checkpoint, str_replace(')','', str_replace('(','',$linha[2])));   //TO-DO resolver para caso onde check tem 2 variaveis
     }
+
     if($linha[0] == "End"){ //Checkpoint
         for($i = 0; count($to_redo) > $i ; $i++){
             if($Checkpoint[0] != $to_redo[$i]){
                 unset($to_redo[$i]);
+                $key = array_search($to_redo[$i], $abertas);
+                unset($abertas[$key]);
             };
         }
-        unset($Checkpoint[0]);
-    }
+        $Checkpoint = [];
+    }   
 
     if($linha[0] == 'commit'){
         if(in_array($linha[1], $abertas)){
@@ -29,23 +38,46 @@ while(!feof($arquivo)){
     if($linha[0] == 'start'){
         array_push($abertas,$linha[1] );
     }
-
 }
 fclose($arquivo);
-print_r($to_redo);
-//rint_r($Checkpoint);
-if (!$db_handle) {
-    echo "An error occurred.\n";
-    exit;
+
+$dont_redu = array_diff($abertas, $to_redo);
+$redu = $to_redo;
+
+$arquivo = fopen ('log.txt', 'r');
+
+while(!feof($arquivo)){ 
+    $linha = fgets($arquivo, 1024);
+    $linha = (str_replace('>','',str_replace('<','',explode(' ',$linha))));
+    if(count($linha) == 1){
+        $altera = (explode(',',$linha[0]));
+        foreach($redu as $value){
+            if($altera[0] == $value){
+                $result = pg_prepare($db_handle, "update table table set $a = $valor where id = $id");
+                $result = pg_execute($db_handle,  array($a = $altera[2],$valor =  $altera[3],$id = $altera[1]));
+    
+                if (!$result) {
+                    echo "An error occurred.\n";
+                    exit;
+                }
+            }
+        }
+    };
+
+    
+        
+
+}   
+fclose($arquivo);
+
+
+foreach($dont_redu as $value){
+   // echo('não fez redu' .' '. $value);
 }
 
-$result = pg_query($db_handle, "SELECT * FROM tabela");
-  if (!$result) {
-    echo "An error occurred.\n";
-    exit;
-  }
-  while ($row = pg_fetch_row($result)) {
-        //print_r($row);
-  }
+foreach($redu as $value){
+   // echo('Fez redu' .' '. $value);
+}
+
 
 ?>
